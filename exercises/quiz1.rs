c15204ddc49af61f9a2eb9b382c78b38c7c64427
0xmusashi <musashihp42@gmail.com>
2024-01-14 15:43:20 +0700

    quiz1: done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_of_apple: u32) -> u32 {
+    if num_of_apple > 40 {
+        num_of_apple * 1
+    } else {
+        num_of_apple * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
